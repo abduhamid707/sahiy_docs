da2b484a4697913b288800f34d5a9fe90e6266f6
abduhamid707 <botirovabduhamid1@gmail.com>
2026-09-18 15:13:58 +0500

    chore: remove unused legacy files

--- scratch/fix_admin_password.js
+++
@@ -1,5 +0,0 @@
-db.users.updateMany(
-  { role: "SUPER_ADMIN" },
-  { $set: { password: "$2b$10$FtMP4e4EsidDVdHDG78aZOrcg7Dc0GcajtL99DXHk9Ph79S4mC8BW" } }
-);
-printjson(db.users.find({ role: "SUPER_ADMIN" }, { email: 1, name: 1, role: 1 }).toArray());
